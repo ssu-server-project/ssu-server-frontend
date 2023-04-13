62c95377b61cde2c7b342677d271910dc658cbab
parkch <ckdgusqkr0803@gmail.com>
2023-04-13 19:02:15 +0900

    secret information secure

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const express = require('express')
-// express 모듈을 가져오기
-const app = express()
-// 새로운 앱을 만듬
+const express = require('express')  // express 모듈을 가져오기
+const app = express()  // 새로운 앱을 만듬
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+// config/key.js에서 production에 따른 URI 참조 방식을
+// 결정해줌.
 
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json())
+
+//    "mongoose": "^7.0.3",
+// config 안의 mongoURI를 가져와주는 방식.
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://parkch:12341234@boilerplate.7bsnomf.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,useUnifiedTopology: true
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex : true,
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
-app.get('/', (req, res) =>  res.send('Hello World!'))
+app.get('/', (req, res) =>  res.send('Hello World! 안녕하세요!'))
+
+app.post('/register', async (req, res) => {
+    //회원가입시 필요 정보를 client에서 가져오면
+    //데이터베이스에 삽입한다
+  
+    //body parser를 통해 body에 담긴 정보를 가져온다
+    const user = new User(req.body)
+  
+    //mongoDB 메서드, user모델에 저장
+    const result = await user.save().then(()=>{
+      res.status(200).json({
+        success: true
+      })
+    }).catch((err)=>{
+      res.json({ success: false, err })
+    })
+  })
 
 app.listen(port, () =>   console.log(`Example app listening on port ${port}`))
